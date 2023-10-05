00357a9ee1d9c53dc324cd410a69fd7d2b47680d
Domitory1 <jomunyts@gmail.com>
2023-10-05 15:27:26 +0300

    modified:   index.html new file:   js/code.jquery.com_jquery-3.7.1.min.js deleted:    js/jquery-3.7.1.js deleted:    js/jquery.touchSwipe.min.js modified:   js/script.js new file:   js/slick-1.8.1/.gitignore new file:   js/slick-1.8.1/CONTRIBUTING.markdown new file:   js/slick-1.8.1/ISSUE_TEMPLATE.md new file:   js/slick-1.8.1/LICENSE new file:   js/slick-1.8.1/Makefile new file:   js/slick-1.8.1/README.markdown new file:   js/slick-1.8.1/bower.json new file:   js/slick-1.8.1/component.json new file:   js/slick-1.8.1/index.html new file:   js/slick-1.8.1/package.json new file:   js/slick-1.8.1/slick.jquery.json new file:   js/slick-1.8.1/slick/ajax-loader.gif new file:   js/slick-1.8.1/slick/config.rb new file:   js/slick-1.8.1/slick/fonts/slick.eot new file:   js/slick-1.8.1/slick/fonts/slick.svg new file:   js/slick-1.8.1/slick/fonts/slick.ttf new file:   js/slick-1.8.1/slick/fonts/slick.woff new file:   js/slick-1.8.1/slick/slick-theme.css new file:   js/slick-1.8.1/slick/slick-theme.less new file:   js/slick-1.8.1/slick/slick-theme.scss new file:   js/slick-1.8.1/slick/slick.css new file:   js/slick-1.8.1/slick/slick.js new file:   js/slick-1.8.1/slick/slick.less new file:   js/slick-1.8.1/slick/slick.min.js new file:   js/slick-1.8.1/slick/slick.scss modified:   style.css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-$(function() {
-	$("#myList").swipe({
-	  swipeLeft: function() {
-		 $(this).scrollLeft($(this).scrollLeft() + 200);
-	  },
-	  swipeRight: function() {
-		 $(this).scrollLeft($(this).scrollLeft() - 200);
-	  },
-	  threshold: 0
+$(document).ready(function(){
+	$('.slider').slick({
+		variableWidth: true,
+		arrows: false,
+		infinite: false
 	});
- });
+});
