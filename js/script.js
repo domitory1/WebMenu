018bda39a4c3de4aeb59c513bdf947a25be87a03
Domitory1 <jomunyts@gmail.com>
2023-10-16 13:49:32 +0300

    modified:   css/style.css new file:   img/img.jpeg modified:   index.html modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 new Swiper('.menu-slider', {
-	freeMode: true,
+	freeMode:true,
 	autoWidth: true,
 	slidesPerView: 'auto',
+	centeredSlides: true,
+	centeredSlidesBounds: true,
+	on: {
+		click:function(){
+			if (!this.isBeginnig && !this.isEnd){
+				this.slideTo(this.clickedIndex);
+			}
+		}
+	}
 });
