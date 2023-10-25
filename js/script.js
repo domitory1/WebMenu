5e310c7a445e0da0b5cc53c3a322dcbc6209e379
Domitory1 <jomunyts@gmail.com>
2023-10-25 19:50:51 +0300

    modified:   css/style.css modified:   index.html modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-new Flickity('.container',{
+var header = new Flickity('.slider',{
+	contain: true,
+	freeScroll: true,
 	prevNextButtons: false,
 	pageDots: false,
-	freeScroll: true,
-	contain: true,
 });
 
-new Flickity('.header',{
-	asNavFor: '.container',
-	contain: true,
-	freeScroll: true,
-	prevNextButtons: false,
-	pageDots: false,
+/*Центрирование слайда*/
+var slides = document.querySelectorAll('.categoryName');
+slides.forEach(function(slide, index){
+	slide.addEventListener('click', function(){
+		header.select(index);
+		header.scrollToCell(index, true, true);
+	});
 });
