d54620f90a1b4a0e326d60e71f8a76d62e9911dc
Domitory1 <jomunyts@gmail.com>
2023-10-26 18:52:03 +0300

    modified:   css/style.css modified:   index.html modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Слайдер
 var header = new Flickity('.slider',{
 	contain: true,
 	freeScroll: true,
@@ -5,7 +6,7 @@ var header = new Flickity('.slider',{
 	pageDots: false,
 });
 
-/*Центрирование слайда*/
+//Центрирование слайда
 var slides = document.querySelectorAll('.categoryName');
 slides.forEach(function(slide, index){
 	slide.addEventListener('click', function(){
@@ -13,3 +14,34 @@ slides.forEach(function(slide, index){
 		header.scrollToCell(index, true, true);
 	});
 });
+
+//Плавный переход по якорям
+function smoothScroll(target) {
+	const targetElement = document.querySelector(target);
+	const startPosition = window.scrollY;
+	const targetPosition = targetElement.getBoundingClientRect().top + startPosition;
+	const distance = targetPosition - startPosition;
+	const duration = 700; // Длительность анимации в миллисекундах
+	let startTime = null;
+ 
+	function animation(currentTime) {
+	  if (startTime === null) {
+		 startTime = currentTime;
+	  }
+	  const elapsedTime = currentTime - startTime;
+	  const scrollY = ease(elapsedTime, startPosition, distance, duration);
+	  window.scrollTo(0, scrollY);
+	  if (elapsedTime < duration) {
+		 requestAnimationFrame(animation);
+	  }
+	}
+ 
+	function ease(t, b, c, d) {
+	  t /= d / 2;
+	  if (t < 1) return c / 2 * t * t + b;
+	  t--;
+	  return -c / 2 * (t * (t - 2) - 1) + b;
+	}
+ 
+	requestAnimationFrame(animation);
+ }
