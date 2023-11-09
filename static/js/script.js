9cd9732d0f832afe10070c73addb499db20ef9be
Domitory1 <jomunyts@gmail.com>
2023-11-09 18:58:46 +0300

    modified:   index.html modified:   static/js/script.js modified:   static/style/style.css

--- static/js/script.js
+++ static/js/script.js
@@ -2,11 +2,30 @@ tg = window.Telegram.WebApp;
 tg.expand();
 tg.enableClosingConfirmation();
 
-document.addEventListener('DOMContentLoaded', () => {
+let phoneInput = document.querySelector('input');
+let button = document.querySelector('button');
 
-    const inputElement = document.querySelector('input')
-    const maskOptions = {
-      mask: '+{7} (000) 000-00-00'
-    }
-    IMask(inputElement, maskOptions)
-})
+const phoneMask = new IMask(phoneInput, {
+  mask: "+{7} (000) 000-00-00"
+});
+
+phoneInput.addEventListener("input", phoneInputHandler);
+button.addEventListener("click", buttonHandler);
+
+function phoneInputHandler(){
+  if (phoneMask.masked.isComplete){
+    button.classList.add("button--active");
+  } else{
+    button.classList.remove("button--active");
+  }
+}
+
+
+// !По необходимости изменить send_msg.php
+async function buttonHandler(e){
+  e.preventDefault();
+  return await fetch("send_msg.php", {
+    method: "POST",
+    body: phoneMask.unmaskedValue
+  })
+}
