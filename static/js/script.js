4d810c73e07f172d54746d1a994defa4d04e8c0e
Domitory1 <jomunyts@gmail.com>
2023-11-13 18:50:46 +0300

    modified:   static/js/script.js modified:   static/style/style.css

--- static/js/script.js
+++ static/js/script.js
@@ -3,37 +3,29 @@ tg.expand();
 tg.enableClosingConfirmation();
 
 tg.MainButton.text = "Подтвердить номер"
-if(color_btn) {
-	tg.MainButton.color = color_btn;
-}
-
-if(color_text_btn) {
-	tg.MainButton.textColor = color_text_btn;
-}
 
 let phoneInput = document.querySelector('input');
-let button = document.querySelector('button');
 
 const phoneMask = new IMask(phoneInput, {
   mask: "+{7} (000) 000-00-00"
 });
 
-
 phoneInput.addEventListener("input", phoneInputHandler);
-button.addEventListener("click", buttonHandler);
 
 function phoneInputHandler(){
   if (phoneMask.masked.isComplete){
-    button.classList.add("button--active");
-    phoneInput.classList.remove("wrong");
+    tg.MainButton.enable();
   } else{
-    button.classList.remove("button--active");
+    tg.MainButton.disable();
   }
 }
 
+
+
+/*
 async function buttonHandler(e){
   e.preventDefault();
-  if (button.classList == "button--active"){
+  if (tg.MainButtonclassList == "button"){
     return await fetch("send_msg.php", {
       method: "POST",
       body: phoneMask.unmaskedValue
@@ -41,4 +33,4 @@ async function buttonHandler(e){
   } else {
     phoneInput.classList.add("wrong")
   }
-}
+}*/
