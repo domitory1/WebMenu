bc666be97a75793c1ac747c56e3c4c2d2ebd2d76
Domitory1 <jomunyts@gmail.com>
2023-11-14 17:19:42 +0300

    modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,7 @@ const phoneMask = new IMask(phoneInput, {
   mask: "+{7} (000) 000-00-00"
 });
 
-$('#change_search_input').mouseup(function(e){
+$('input').mouseup(function(e){
   e.preventDefault();
   console.log("скрытие");
 });
