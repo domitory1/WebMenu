df7a473ad237c5f7186205a398cfc9e005adbd00
Domitory1 <jomunyts@gmail.com>
2023-11-19 13:44:16 +0300

    modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,38 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 tg.enableClosingConfirmation();
-tg.MainButton.hideProgress();
-
 tg.MainButton.text = "Подтвердить номер";
 tg.MainButton.show();
-let phoneInput = document.querySelector('input');
-/*let flag = true;*/
 
-const phoneMask = new IMask(phoneInput, {
-  mask: "+7 (000) 000-00-00"
-});
-
+// Эта часть кода необходима для айфов. Без нее клавиатура не скрывается по нажанию вне поля input
+let flag = true;
 document.addEventListener( 'click', (e) => {
 	const withinBoundaries = e.composedPath().includes(phoneInput);
 	if (! withinBoundaries ) {
 		phoneInput.blur();
 	} else{
     phoneInput.focus();
-    /*
     if (flag){
       phoneInput.value = '+7 (';
       flag = false;
     }
-    */
   }
 });
 
-phoneInput.addEventListener("input", phoneInputHandler);
+let phoneInput = document.querySelector('input');
 
-function phoneInputHandler(){
+const phoneMask = new IMask(phoneInput, {
+  mask: "{+7} (000) 000-00-00"
+});
+
+phoneInput.addEventListener('input', function(){
   if (phoneMask.masked.isComplete){
     phoneInput.classList.add('correctInput');
     phoneInput.classList.remove('incorrectInput');
   } else{
     phoneInput.classList.remove('correctInput');
   }
-}
+});
 
 tg.MainButton.onClick(function(){
   phoneInput.blur();
