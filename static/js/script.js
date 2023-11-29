0fab4365494663f1114f719269b2d9657b8b68b4
Domitory1 <jomunyts@gmail.com>
2023-11-29 18:53:44 +0300

    modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -168,5 +168,5 @@ tg.MainButton.on('click', function(){
 		title: '🤔',
 		message: "Переход в корзину"
 	});
-	window.location.href = 'https://domitory1.github.io/Basket//Basket.html';
+	window.location.href = 'https://domitory1.github.io/Basket/Basket.html';
 });
