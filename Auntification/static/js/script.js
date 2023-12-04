fefbcd3e1053a646217d0f2729e82d133dd204de
Domitory1 <jomunyts@gmail.com>
2023-12-04 12:12:07 +0300

    new file:   Auntification/index.html renamed:    static/img/logo.png -> Auntification/static/img/logo.png renamed:    static/js/imask.js -> Auntification/static/js/imask.js renamed:    Menu/static/js/jquery.min.js -> Auntification/static/js/jquery.min.js new file:   Auntification/static/js/script.js renamed:    Menu/static/js/telegram-web-app.js -> Auntification/static/js/telegram-web-app.js renamed:    static/style/style.css -> Auntification/static/style/style.css new file:   Basket/static/img/cyber-technology-background_23-2148390330.avif deleted:    Menu/index.html deleted:    Menu/static/js/script.js modified:   index.html renamed:    Menu/static/css/flickity.min.css -> static/css/flickity.min.css renamed:    Menu/static/css/style.css -> static/css/style.css renamed:    Menu/static/img/img.jpeg -> static/img/img.jpeg renamed:    Menu/static/img/img.png -> static/img/img.png renamed:    Menu/static/js/flickity.pkgd.min.js -> static/js/flickity.pkgd.min.js modified:   static/js/script.js

---
+++ Auntification/static/js/script.js
@@ -0,0 +1,55 @@
+let tg = window.Telegram.WebApp;
+
+tg.ready();
+tg.expand();
+tg.enableClosingConfirmation();
+tg.MainButton.text = "Подтвердить номер";
+tg.MainButton.show();
+
+// Эта часть кода необходима для айфов. Без нее клавиатура не скрывается по нажанию вне поля input
+let flag = true;
+document.addEventListener( 'click', (e) => {
+	const withinBoundaries = e.composedPath().includes(phoneInput);
+	if (! withinBoundaries ) {
+		phoneInput.blur();
+	} else{
+    phoneInput.focus();
+    if (flag){
+      phoneInput.value = '+7 (';
+      flag = false;
+    }
+  }
+});
+
+let phoneInput = document.querySelector('input');
+
+const phoneMask = new IMask(phoneInput, {
+  mask: "{+7} (000) 000-00-00"
+});
+
+phoneInput.addEventListener('input', function(){
+  if (phoneMask.masked.isComplete){
+    phoneInput.classList.add('correctInput');
+    phoneInput.classList.remove('incorrectInput');
+    p.classList.remove("show");
+  } else{
+    phoneInput.classList.remove('correctInput');
+  }
+});
+
+let p = document.querySelector('.secondP');
+
+tg.MainButton.onClick(function(){
+  phoneInput.blur();
+  if (phoneInput.classList == 'correctInput'){
+    fetch("send_msg.php", {
+      method: "POST",
+      body: phoneMask.unmaskedValue
+    })
+    sessionStorage.setItem('number', phoneInput.value);
+    window.location.href = 'https://domitory1.github.io/checkCode/SendCode.html';
+  } else{
+    phoneInput.classList.add('incorrectInput');
+    p.classList.add('show');
+  }
+});
