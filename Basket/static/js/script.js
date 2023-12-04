8a9bc6dda8b57f43393ed1e69b641a379926c5a1
Domitory1 <jomunyts@gmail.com>
2023-12-04 12:28:35 +0300

    modified:   Basket/static/js/script.js

--- Basket/static/js/script.js
+++ Basket/static/js/script.js
@@ -5,3 +5,8 @@ tg.expand();
 tg.enableClosingConfirmation();
 tg.MainButton.text = "Заказать";
 tg.MainButton.show();
+tg.BackButton.show();
+tg.BackButton.onClick(function(){
+    tg.BackButton.hide();
+    window.history.back();
+})
