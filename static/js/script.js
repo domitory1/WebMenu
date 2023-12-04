32b231a339d636b00e542d8dda02360d33f43161
Domitory1 <jomunyts@gmail.com>
2023-12-04 13:14:12 +0300

    modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -52,7 +52,7 @@ tg.MainButton.onClick(function(){
   } else{
     phoneInput.classList.add('incorrectInput');
     console.log(phoneInput.value);
-    if (phoneInput.value == ''){
+    if (phoneInput.value == ""){
       p[0].innerHtml = "Введите номер телефона";
       p.classList.add('show');
    } else{
