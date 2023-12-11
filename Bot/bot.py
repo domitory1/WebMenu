0369ca15da10259ee9ddae33c97007e042192761
Domitory1 <jomunyts@gmail.com>
2023-12-11 16:25:40 +0300

    renamed:    index.html -> Auntification/index.html renamed:    static/img/logo.png -> Auntification/static/img/logo.png renamed:    static/js/imask.js -> Auntification/static/js/imask.js renamed:    Menu/static/js/jquery.min.js -> Auntification/static/js/jquery.min.js new file:   Auntification/static/js/script.js renamed:    Menu/static/js/telegram-web-app.js -> Auntification/static/js/telegram-web-app.js renamed:    static/style/style.css -> Auntification/static/style/style.css modified:   Bot/bot.py deleted:    Menu/static/js/script.js renamed:    Menu/main.html -> main.html renamed:    Menu/static/css/flickity.min.css -> static/css/flickity.min.css renamed:    Menu/static/css/style.css -> static/css/style.css renamed:    Menu/static/img/img.jpeg -> static/img/img.jpeg renamed:    Menu/static/js/flickity.pkgd.min.js -> static/js/flickity.pkgd.min.js modified:   static/js/script.js

--- Bot/bot.py
+++ Bot/bot.py
@@ -3,23 +3,21 @@ import logging
 import sys
 import time
 
-from aiogram import Bot, Dispatcher, Router, F
+from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from aiogram.utils.markdown import hbold
 
 import Button.button as button
 
 API_TOKEN = "6658531652:AAEn5TRw5p4yHulphWYttyNfr2bQecvDNvU"
 
 dp = Dispatcher()
-router = Router()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
    await message.answer("Добрый день! 😊")
-   time.sleep(2)
+   time.sleep(1)
    # !Заменить имя бота
    await message.answer("Меня зовут WebAppTestBot.\n Я был создан командой 12XT для того, "
                         "чтобы вы могли сделать удаленный заказ в студенческой столовой Меридиан.")
@@ -27,9 +25,11 @@ async def command_start_handler(message: Message) -> None:
    await message.answer("Перед тем, как вы сделаете ваш первый заказ, поделитесь, пожалуйста, своим номером телефона. 😌\n\n"
                         "Он нужен для того, чтобы вы смогли получить свой заказ.", reply_markup=button.reply_number_phone)
 
-@router.message.contact()
+@dp.message()
 async def contact_handler(message: Message):
-    await message.answer("Работает")
+   if  message.contact is not None:
+      print(message.contact.phone_number)
+      await message.answer(text="Отлично", reply_markup=types.ReplyKeyboardRemove())
 
 async def main() -> None:
    bot = Bot(API_TOKEN, parse_mode=ParseMode.HTML)
